9e3a52cd0f1f400ad70e811e1268891b48acf1ee
Tyler Collins <tjscollins@gmail.com>
2017-02-05 11:08:33 +1000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "ecmaFeatures": {
+    "classes": true,
+    "jsx": true,
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+  },
+  "extends": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+  },
+  "plugins": [
+  ],
+  "rules": {
+  },
+  "settings": {
+  }
+};
---
+++ index.android.js
@@ -0,0 +1,9 @@
+// Index.android.js -- place code in here for Android
+
+/*----------Modules----------*/
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a Component
+
+// Render Component
