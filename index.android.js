015683cd7e796e3199ed7dfb314cbd06597e5a70
Tyler Collins <tjscollins@gmail.com>
2017-03-20 19:32:44 +1000

    Disabling MapViewer for now due to broken build

--- index.android.js
+++ index.android.js
@@ -7,7 +7,7 @@ import {AppRegistry, View} from 'react-native';
 /*----------Components----------*/
 import Header from './src/components/Header';
 import FeatureList from './src/components/FeatureList';
-import MapViewer from './src/components/MapViewer';
+// import MapViewer from './src/components/MapViewer';
 
 // Create a Component
 const App = () =>  (
