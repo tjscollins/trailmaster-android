27489375771782f81a9aeabd87fe470c84ed36ce
Tyler Collins <tjscollins@gmail.com>
2017-03-23 12:08:31 +1000

    Got basic MapViewer working

--- android/app/src/main/java/com/trailmaster/MainApplication.java
+++ android/app/src/main/java/com/trailmaster/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -20,6 +21,15 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new MapsPackage(),
+          new VectorIconsPackage()
+      );
     }
   };
 
--- index.android.js
+++ index.android.js
@@ -2,20 +2,20 @@
 
 /* ----------Modules---------- */
 import React from 'react';
-import {AppRegistry, View} from 'react-native';
+import {AppRegistry, View, Text} from 'react-native';
 
 /*----------Components----------*/
 import Header from './src/components/Header';
 import FeatureList from './src/components/FeatureList';
-// import MapViewer from './src/components/MapViewer';
+import MapViewer from './src/components/MapViewer';
 
 // Create a Component
 const App = () =>  (
-  <View>
+  <View style={{flex: 1}}>
     <Header headerText={'Trailmaster'}/>
     {/* <SearchModal /> */}
-    <FeatureList />
-    {/* <MapViewer /> */}
+    {/* <FeatureList /> */}
+    <MapViewer />
   </View>
 );
 
