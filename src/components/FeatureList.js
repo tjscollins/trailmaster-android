99e49cb5daeebff44e6069f0d74826062349bb8e
Tyler Collins <tjscollins@gmail.com>
2017-03-28 19:44:24 +1000

    Implement regexp searching in FeatureList

--- src/components/FeatureList.js
+++ src/components/FeatureList.js
@@ -1,6 +1,6 @@
 /*----------Modules----------*/
 import React, {Component} from 'react';
-import {View, ScrollView, Text} from 'react-native';
+import {View, ScrollView, TextInput} from 'react-native';
 import {connect} from 'react-redux';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
@@ -15,25 +15,44 @@ import Header from './Header';
 // import {fetchData, validateServerData} from '../api/TrailmasterAPI';
 
 class FeatureList extends Component {
+  state = {
+    searchText: '',
+  }
   renderGeoJSON() {
-    // console.log('FeatureList: ', this.props.geoJSON.features)
     return this
       .props
       .geoJSON
       .features
+      .filter(({properties:{name}}) => name.match(new RegExp(this.state.searchText, 'i')))
       .map((feature) => <FeatureDetail key={feature._id + 'geoJSON-list'} feature={feature}/>);
   }
   render() {
     const styles = EStyleSheet.create({
       scrollViewStyle: {},
+      searchBoxStyle: {
+        height: 50,
+        backgroundColor: 'white',
+        marginBottom: 0,
+        borderBottomWidth: 1,
+        borderColor: '#f0f0f0',
+      },
+      searchTextInput: {
+        textAlign: 'center',
+      }
     });
     return (
       <View style={{
         flex: 1
       }}>
         <Header toRoute={this.props.replaceRoute} headerText={'Trailmaster'}/>
+        <View style={styles.searchBoxStyle}>
+          <TextInput
+            placeholder={'Search'}
+            value={this.state.searchText}
+            style={styles.searchTextInput}
+            onChangeText={(searchText) => this.setState({searchText})} />
+        </View>
         <ScrollView style={styles.scrollViewStyle}>
-          {/* <Text>{JSON.stringify(this.props.geoJSON.features[0])}</Text> */}
           {this.renderGeoJSON()}
         </ScrollView>
       </View>
