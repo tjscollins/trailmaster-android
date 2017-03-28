28a78221c2b53d2f463813ed2f5b47f88e256eeb
Tyler Collins <tjscollins@gmail.com>
2017-03-28 16:28:14 +1000

    Bugfix for TrailList loading indicator

--- src/components/TrailList.js
+++ src/components/TrailList.js
@@ -20,70 +20,53 @@ class TrailList extends Component {
   constructor() {
     super();
     this.state = {
-      featureList: [],
       trails: []
     }
-    loaderHandler.showLoader('Loading');
+  }
+  loadTrailsFromProps(props) {
+    const myTrails = props ? props.trails.myTrails : this.props.trails.myTrails;
+    if(this.state.trails.length !== myTrails.length) {
+      const trails = myTrails.map((trail) => <TrailDetail
+        replaceRoute={this.props.replaceRoute}
+        key={trail._id + 'trail-list'}
+        trail={trail}/>);
+        if (trails.length > 0) {
+          loaderHandler.hideLoader();
+        }
+        this.setState({trails});
+      } else if (this.state.trails.length === 0) {
+        const {xAuth, email} = this.props.userSession;
+        const {trails, dispatch} = this.props;
+        axios
+        .get('https://trailmaster.herokuapp.com/trails', {
+          headers: {
+            'x-auth': xAuth
+          }
+        })
+        .then((response) => {
+          const newTrails = response.data.trails;
+          if (newTrails.length !== trails.myTrails.length) {
+            AsyncStorage.setItem(`trails-${email}`, JSON.stringify(newTrails));
+            dispatch(actions.displayTrails(newTrails));
+            }
+          })
+          .catch((error) => {
+            console.error('Error fetching trails: ', error);
+          });
+      }
   }
   componentWillMount() {
-    if(this.state.trails.length !== this.props.trails.myTrails.length) {
-      console.log('TrailList: ', this.state.trails, typeof this.props.trails.myTrails)
-      const trails = this.props.trails.myTrails.map((trail) => <TrailDetail
-        replaceRoute={this.props.replaceRoute}
-        key={trail._id + 'trail-list'}
-        trail={trail}/>);
-        if (trails.length > 0) {
-          loaderHandler.hideLoader();
-        }
-        this.setState({trails});
-    }
-  }
-  componentDidMount() {
-    const {xAuth, email} = this.props.userSession;
-    const {trails, dispatch, replaceRoute} = this.props;
-    axios
-      .get('https://trailmaster.herokuapp.com/trails', {
-      headers: {
-        'x-auth': xAuth
-      }
-    })
-      .then((response) => {
-        const newTrails = response.data.trails;
-        if (newTrails.length !== trails.myTrails.length) {
-          AsyncStorage.setItem(`trails-${email}`, JSON.stringify(newTrails));
-          dispatch(actions.displayTrails(newTrails));
-          const trailList = newTrails.map((trail) => <TrailDetail
-            replaceRoute={replaceRoute}
-            key={trail._id + 'trail-list'}
-            trail={trail}/>);
-          this.setState({trails: trailList});
-        }
-      })
-      .catch((error) => {
-        console.log('Error fetching trails: ', error);
-      })
+    loaderHandler.showLoader('Loading');
+    this.loadTrailsFromProps(null);
   }
   componentWillReceiveProps(nextProps) {
-    if(this.state.trails.length !== nextProps.trails.myTrails.length) {
-      const trails = nextProps
-      .trails
-      .myTrails
-      .map((trail) => <TrailDetail
-        replaceRoute={this.props.replaceRoute}
-        key={trail._id + 'trail-list'}
-        trail={trail}/>);
-        if (trails.length > 0) {
-          loaderHandler.hideLoader();
-        }
-        this.setState({trails});
-    }
+    this.loadTrailsFromProps(nextProps);
   }
   render() {
     const styles = EStyleSheet.create({scrollViewStyle: {}});
     return (
       <View style={{flex: 1}}>
         <Header toRoute={this.props.replaceRoute} headerText={'Trailmaster'}/>
-
         <ScrollView style={styles.scrollViewStyle}>
           {this.state.trails}
         </ScrollView>
